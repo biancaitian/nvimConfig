0443c05b1a2ebc45ab56475161b639bad4ec6456
吴云龙 <2211548693@qq.com>
2025-06-08 17:45:12 +0800

    Changes to be committed: 	new file:   nvim/lua/plugin/comment.lua

---
+++ nvim/lua/plugin/comment.lua
@@ -0,0 +1,18 @@
+return{
+        'numToStr/Comment.nvim',
+        opts = {
+                toggler = {
+                    ---Line-comment toggle keymap
+                    -- line = "<leader>c",
+                    line = "<leader>cl",
+                    ---Block-comment toggle keymap
+                    block = "<leader>cb",
+                },
+                opleader = {
+                    ---Line-comment keymap
+                    line = "<leader>ccl",
+                    ---Block-comment keymap
+                    block = "<leader>ccb",
+                },
+        },
+}
